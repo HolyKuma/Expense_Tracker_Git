1d5da0ce484ff2c680c7d144af6279b5c6bcd316
Maximilian Eder <Maximilian.eder97@gmail.com>
2024-08-31 23:00:39 +0200

    small changes

--- frontend/src/Components/Expenses/ExpenseForm.js
+++ frontend/src/Components/Expenses/ExpenseForm.js
@@ -17,7 +17,7 @@ function ExpenseForm() {
         description: '',
     })
 
-    const { title, amount, date, category,description } = inputState;
+    const { title, amount, date, category, description } = inputState;
 
     const handleInput = name => e => {
         setInputState({...inputState, [name]: e.target.value})
