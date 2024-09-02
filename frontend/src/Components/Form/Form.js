f3d637c13f8945574b828da5717fee0452594b25
Maximilian Eder <Maximilian.eder97@gmail.com>
2024-09-02 16:55:51 +0200

    dateFormat

--- frontend/src/Components/Form/Form.js
+++ frontend/src/Components/Form/Form.js
@@ -61,7 +61,7 @@ function Form() {
                     id='date'
                     placeholderText='Enter A Date'
                     selected={date}
-                    dateFormat="MM/yyyy"
+                    dateFormat="dd/MM/yyyy"
                     onChange={(date) => {
                         setInputState({...inputState, date: date})
                     }}
